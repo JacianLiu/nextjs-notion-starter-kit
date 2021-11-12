ead5c9e5dc6cacf7f70f1ff60a8114f3fdb48815
Jacian <45160423+JacianLiu@users.noreply.github.com>
2021-11-12 23:39:50 +0800

    Update get-canonical-page-id.ts

--- lib/get-canonical-page-id.ts
+++ lib/get-canonical-page-id.ts
@@ -22,18 +22,16 @@ export function getCanonicalPageId(
   } else {
     var url = getCanonicalPageIdImpl(pageId, recordMap, {
       uuid
-    }) + randomString(5);
-    console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
-    console.log(url)
-    return url
-  }
-}
-
-function randomString(e) {    
-    e = e || 32;
+    });
     var t = "ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678",
     a = t.length,
     n = "";
-    for (i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * a));
-    return n
+    for (i = 0; i < 6; i++) {
+      n += t.charAt(Math.floor(Math.random() * a));
+    }
+    console.log("uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu")
+    console.log(url + n)
+    return url + n
+  }
 }
+
